b756a561f94b8f484645e12dff456d82203da07e
zhanss <1579255101@qq.com>
2022-10-20 10:48:12 +0800

    新增ah-security项目

---
+++ ah-security/src/main/java/com/zhansc/ahsecurity/AhSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.zhansc.ahsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhSecurityApplication.class, args);
+    }
+
+}
---
+++ ah-security/src/test/java/com/zhansc/ahsecurity/AhSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhansc.ahsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
