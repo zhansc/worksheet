ad8832f73395fd568891ff8f3c85c4885f156e7e
zhansc <1579255101@qq.com>
2022-10-29 22:17:04 +0800

    新增雏菊模块-学习

---
+++ daisy/src/main/java/cn/com/zhansc/daisy/DaisyApplication.java
@@ -0,0 +1,13 @@
+package cn.com.zhansc.daisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaisyApplication.class, args);
+    }
+
+}
